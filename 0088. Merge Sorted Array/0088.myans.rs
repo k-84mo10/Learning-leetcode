d633b6cbc9bd59d6656c126f99318264c7cd68f9
k-84mo10 <kokiaptx0812@gmail.com>
2025-04-28 01:35:04 +0900

    add 0088 rust

---
+++ 0088. Merge Sorted Array/0088.myans.rs
@@ -0,0 +1,28 @@
+impl Solution {
+    pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+        if n == 0 {
+            return;
+        }
+
+        let mut p1 = m-1;
+        let mut p2 = n-1;
+        let mut p = m+n-1;
+
+        while p1 >= 0 && p2 >= 0 {
+            if nums1[p1 as usize] > nums2[p2 as usize] {
+                nums1[p as usize] = nums1[p1 as usize];
+                p1 -= 1;
+            } else {
+                nums1[p as usize] = nums2[p2 as usize];
+                p2 -= 1;
+            }
+            p -= 1;
+        }
+
+        while p2 >= 0 {
+            nums1[p as usize] = nums2[p2 as usize];
+            p -= 1;
+            p2 -= 1;
+        }
+    }
+}
