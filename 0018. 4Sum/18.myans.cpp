b43d626f633fdaa8d2b65e52ba3b084b3929cb7e
k-84mo10 <kokiaptx0812@gmail.com>
2025-05-22 11:16:10 +0900

    add 18

---
+++ 0018. 4Sum/18.myans.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        std::sort(nums.begin(), nums.end());
+        std::vector<std::vector<int>> answer;
+        int n = nums.size();
+
+        for (int i=0; i<n-3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+
+            for (int j=i+1; j<n-2; j++) {
+                if (j > i+1 && nums[j] == nums[j-1]) continue;
+
+                int left = j+1, right = n-1;
+                while (left < right) {
+                    long long four_sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (four_sum == target) {
+                        answer.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left+1]) left += 1;
+                        while (left < right && nums[right] == nums[right-1]) right -= 1;
+                    }
+                    
+                    if (four_sum >= target) right -= 1;
+                    if (four_sum <= target) left += 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+};
