d740a1f4b44f33b1c5b2d243fd854bd46c375e47
k-84mo10 <kokiaptx0812@gmail.com>
2025-08-02 16:53:57 +0900

    fix 0240

--- 0240. Search a 2D Matrix II/0240.myans.rs
+++ 0240. Search a 2D Matrix II/0240.myans.rs
@@ -5,14 +5,13 @@ impl Solution {
 
         let (mut current_i, mut current_j) = (0, n-1);
 
-        while current_i < m {
+        while current_i < m && current_j < usize::MAX {
             let current = matrix[current_i][current_j];
             if current == target {
                 return true;
             } else if current < target {
                 current_i += 1;
             } else {
-                if current_j == 0 { break; }
                 current_j -= 1;
             }
         }
